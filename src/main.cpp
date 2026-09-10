4792099c6473edec872c0186e5fda984a807ea17
Allan Carpenter <83547562+acarpenter314@users.noreply.github.com>
2026-09-10 14:47:58 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,30 @@
-srand(time(nullptr));
-    Deck deck;
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
-    bool playAgain = true;
+// ------------------------------------------------------------
+// Card Class
+// Represents a single playing card with a suit and value.
+// ------------------------------------------------------------
+class Card {
+public:
+    std::string suit;
+    std::string value;
 
-    while (playAgain) {
-        std::cout << "\n=== Guess The Card (C++ Edition) ===\n";
-        std::cout << "Choose difficulty:\n";
-        std::cout << "1. Guess Suit\n";
-        std::cout << "2. Guess Value\n";
-        std::cout << "3. Guess Both\n";
-        std::cout << "Enter choice: ";
+    // Constructor to create a card with a suit and value
+    Card(std::string s, std::string v) : suit(s), value(v) {}
+};
 
-        int choice;
-        std::cin >> choice;
+// ------------------------------------------------------------
+// Deck Class
+// Holds a full deck of 52 cards and can return a random card.
+// ------------------------------------------------------------
+class Deck {
+private:
+    std::vector<Card> cards;  // Dynamic list of all cards
 
-        Card card = deck.getRandomCard();
-
-        std::string guessSuit, guessValue;
-
-        if (choice == 1) {
-            std::cout << "Guess the suit (Hearts, Diamonds, Clubs, Spades): ";
-            std::cin >> guessSuit;
-
-            if (guessSuit == card.suit) {
-                std::cout << "Correct! The card was " << card.value << " of " << card.suit << ".\n";
-            } else {
-                std::cout << "Wrong! The card was " << card.value << " of " << card.suit << ".\n";
-            }
-
-        } else if (choice == 2) {
-            std::cout << "Guess the value (Ace, 2-10, Jack, Queen, King): ";
-            std::cin >> guessValue;
-
-            if (guessValue == card.value) {
-                std::cout << "Correct! The card was " << card.value << " of " << card.suit << ".\n";
-            } else {
-                std::cout << "Wrong! The card was " << card.value << " of " << card.suit << ".\n";
-            }
-
-        } else if (choice == 3) {
-            std::cout << "Guess the suit: ";
-            std::cin >> guessSuit;
-            std::cout << "Guess the value: ";
-            std::cin >> guessValue;
-
-            if (guessSuit == card.suit && guessValue == card.value) {
-                std::cout << "Perfect! You guessed both correctly!\n";
-            } else {
-                std::cout << "Wrong! The card was " << card.value << " of " << card.suit << ".\n";
-            }
-
-        } else {
-            std::cout << "Invalid choice.\n";
-        }
-
-        char again;
-        std::cout << "\nPlay again? (y/n): ";
-        std::cin >> again;
-
-        if (again != 'y' && again != 'Y') {
-            playAgain = false;
-        }
-    }
-
-    std::cout << "\nThanks for playing!\n";
-    return 0;
-}
+public:
+    // Constructor builds the full deck automatically
+    Deck() {
+        std::vector<std::string> suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
